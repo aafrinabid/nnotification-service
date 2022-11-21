4e59223800f8e517c776fbd3f0bb5d0b6ad8c41c
aafrinabid <mohdaafrin@gmail.com>
2022-11-21 11:19:41 +0530

    edited the test files in task service for the task repository

--- src/task/task.service.spec.ts
+++ src/task/task.service.spec.ts
@@ -1,18 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { Task } from './task.entity';
-import { TaskRepository } from './task.repository';
 import { TaskService } from './task.service';
+import { TasksRepository } from './tasks.repository';
 
 describe('TaskService', () => {
   let service: TaskService;
-  let mockRepositroy = TaskRepository
+  let mockRepositroy: TasksRepository
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TaskService],
+      providers: [TaskService, TasksRepository],
     }).compile();
 
     service = module.get<TaskService>(TaskService);
+    mockRepositroy = module.get<TasksRepository>(TasksRepository)
   });
 
   it('should be defined', () => {
@@ -21,13 +22,13 @@ describe('TaskService', () => {
 
   it('should fetch all pending task where is to be sent', async () =>{
     let fetchedList: Promise<Task[]>;
-    jest.spyOn(mockRepositroy,'fetchAllPendingTasks').mockImplementation(()=>fetchedList)
+    jest.spyOn(mockRepositroy,'FetchAllPendingTasks').mockImplementation(()=>fetchedList)
     expect(await service.fetchAllPendingTasks(new Date)).toBe(fetchedList)
   })
 
   it('should update status of task where mail is sent', async () => {
     let updateStatus: Promise<boolean>;
-    jest.spyOn(mockRepositroy,'updateEmailSentStatus').mockImplementation(()=>updateStatus)
+    jest.spyOn(mockRepositroy,'UpdateEmailSentStatus').mockImplementation(()=>updateStatus)
     expect(await service.updateEmailSentTastus(1)).toBe(updateStatus)
   })
 });
